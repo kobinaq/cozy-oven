ad0c437f649a059b272c3039ea47abfed7e9c698
kobinaq <kobinastudios@gmail.com>
2026-06-24 21:31:34 +0000

    update

--- types/lucide-react.d.ts
+++ types/lucide-react.d.ts
@@ -11,11 +11,13 @@ declare module "lucide-react" {
   >;
 
   export const Accessibility: LucideIcon;
+  export const AlertCircle: LucideIcon;
   export const ArrowLeft: LucideIcon;
   export const ArrowRight: LucideIcon;
   export const Ban: LucideIcon;
   export const BarChart3: LucideIcon;
   export const Bell: LucideIcon;
+  export const Check: LucideIcon;
   export const CheckCircle: LucideIcon;
   export const CheckCircle2: LucideIcon;
   export const CheckSquare: LucideIcon;
@@ -23,8 +25,14 @@ declare module "lucide-react" {
   export const ChevronRight: LucideIcon;
   export const CreditCard: LucideIcon;
   export const Download: LucideIcon;
+  export const Edit2: LucideIcon;
   export const Eye: LucideIcon;
+  export const EyeOff: LucideIcon;
   export const Filter: LucideIcon;
+  export const Gift: LucideIcon;
+  export const Heart: LucideIcon;
+  export const Instagram: LucideIcon;
+  export const KeyRound: LucideIcon;
   export const LayoutDashboard: LucideIcon;
   export const Loader2: LucideIcon;
   export const Lock: LucideIcon;
@@ -33,19 +41,25 @@ declare module "lucide-react" {
   export const MapPin: LucideIcon;
   export const Menu: LucideIcon;
   export const MessageCircle: LucideIcon;
+  export const Minus: LucideIcon;
   export const MoreVertical: LucideIcon;
   export const Package: LucideIcon;
   export const Phone: LucideIcon;
   export const Plus: LucideIcon;
+  export const Quote: LucideIcon;
   export const RefreshCw: LucideIcon;
   export const Save: LucideIcon;
   export const Search: LucideIcon;
   export const Send: LucideIcon;
+  export const ShieldCheck: LucideIcon;
   export const ShoppingBag: LucideIcon;
   export const ShoppingCart: LucideIcon;
+  export const Sparkles: LucideIcon;
   export const Square: LucideIcon;
   export const Star: LucideIcon;
   export const Trash2: LucideIcon;
+  export const TrendingDown: LucideIcon;
+  export const TrendingUp: LucideIcon;
   export const Truck: LucideIcon;
   export const Upload: LucideIcon;
   export const User: LucideIcon;
@@ -53,4 +67,5 @@ declare module "lucide-react" {
   export const UserPlus: LucideIcon;
   export const Users: LucideIcon;
   export const X: LucideIcon;
+  export const XCircle: LucideIcon;
 }
